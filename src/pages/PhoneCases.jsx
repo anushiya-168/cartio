4590144d0fa0b5630356762e21e7b7ac41ddbe9f
Anushiya <anushiya.r1712@gmail.com>
2026-08-18 11:49:34 +0530

    Sync latest fixes to PhoneCases and other pages

--- src/pages/PhoneCases.jsx
+++ src/pages/PhoneCases.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect } from 'react'
 import { phoneModels, phoneCases } from '../services/phoneCaseData'
 import PhoneCaseCard from '../components/PhoneCaseCard'
 import TrustBar from '../components/TrustBar'
